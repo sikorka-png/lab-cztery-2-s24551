579653df548629ccad10dc8916789a51664f64b6
Mateusz Sikora <sikora.mateo@gmail.com>
2022-10-29 11:12:40 +0200

    k

--- collections-filters/src/main/java/org/example/GeneralCalculator.java
+++ collections-filters/src/main/java/org/example/GeneralCalculator.java
@@ -4,6 +4,7 @@ import org.example.interfaces.ICalculate;
 import org.example.model.Person;
 import org.example.queries.search.FunctionsParameters;
 
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -20,9 +21,11 @@ public class GeneralCalculator implements ICalculate {
     @Override
     public double calculate(FunctionsParameters fp, List<Person> data) {
         if (name.equals(fp.getFieldName())) {
-            data.stream()
+            DoubleSummaryStatistics doubleSummaryStatistics = data.stream()
                     .map(function)
+                    .mapToDouble(Number::doubleValue)
                     .summaryStatistics();
+            System.out.println(doubleSummaryStatistics);
         }
         return 0;
     }
